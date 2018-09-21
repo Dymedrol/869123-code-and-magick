b88450c3a8aec7f690035aa6ca9364ee6fd9cc54
Dmitry Kobilskikh <Dmitrykob@gmail.com>
2018-09-22 00:37:08 +0700

    справлены ошибки

--- js/stat.js
+++ js/stat.js
@@ -4,7 +4,7 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var CLOUD_COLOR = '#fff'
+var CLOUD_COLOR = '#fff';
 var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var GAP = 10;
 var TEXT_GAP = 20;
@@ -44,7 +44,6 @@ window.renderStatistics = function (ctx, names, times) {
     var coloumnHeight = (COLUMN_MAX_HIGHT * times[i]) / maxTime;
     var randColor = Math.floor(Math.random() * 9) + 1;
     var columnColor = 'rgba(10, 26, 199, 0.' + randColor + ')';
-    console.log('Цеыет  ' + randColor);
 
     if (names[i] === 'Вы') {
       columnColor = COLUMN_PLAYER_COLOR;
