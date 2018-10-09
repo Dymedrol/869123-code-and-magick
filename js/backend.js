dc2bdf330efd81d8c1bf43dccd1718c886bbe6a0
Dymedrol <Dmitrykob@gmail.com>
2018-10-09 23:25:23 +0700

    функции бекенда вынесены за объект

--- js/backend.js
+++ js/backend.js
@@ -6,39 +6,46 @@
   var SAVE_URL = 'https://js.dump.academy/code-and-magick';
   var REQUEST_STATUS_OK = 200;
 
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === REQUEST_STATUS_OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.open('GET', LOAD_URL);
+    xhr.send();
+  };
+
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === REQUEST_STATUS_OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.open('post', SAVE_URL);
+    xhr.send(data);
+  };
+
+
+
+
   window.backend = {
 
-    load: function (onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
+    load: load,
 
-      xhr.addEventListener('load', function () {
-        if (xhr.status === REQUEST_STATUS_OK) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.open('GET', LOAD_URL);
-      xhr.send();
-    },
-
-    save: function (data, onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === REQUEST_STATUS_OK) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.open('post', SAVE_URL);
-      xhr.send(data);
-    }
+    save: save
   };
 
 })();
